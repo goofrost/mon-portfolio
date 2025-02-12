ac06d445b9583be945a7775f7de99d991efd84c6
goofrost <quentingaronne1@gmail.com>
2025-02-13 00:44:46 +0100

    fin?

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,7 @@
-import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 export const collections = {
 	work: defineCollection({
-		// Load Markdown files in the src/content/work directory.
-		loader: glob({ base: './src/content/work', pattern: '**/*.md', }),
 		schema: z.object({
 			title: z.string(),
 			description: z.string(),
