6ec4a1ab3e2b0a39126d71613045be883e89551e
goofrost <quentingaronne1@gmail.com>
2025-02-13 00:52:44 +0100

    fin?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://goofrost.github.io',
+  site: 'https://github.com/goofrost/mon-portfolio.git',
   base: '/mon-portfolio/',
 });
